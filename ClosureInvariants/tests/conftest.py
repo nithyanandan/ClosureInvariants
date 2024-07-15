5691a7a78a14936df47c88bb98a67293f784ed5b
Nithyanandan Thyagarajan <nithyanandan.t@gmail.com>
2024-07-16 02:17:18 +0800

    Update polarimetric case tests
    From 4426115 (scalar):
    =====================
    
    (*) Update tests/conftest.py for polarimetric case
    (*) Update tests/test_vectorInvariants.py for polarimetric case

--- ClosureInvariants/tests/conftest.py
+++ ClosureInvariants/tests/conftest.py
@@ -38,7 +38,7 @@ def antenna_pairs(antenna_ids):
     return [(antenna_ids[i], antenna_ids[j]) for i in range(len(antenna_ids)) for j in range(i + 1, len(antenna_ids))]
 
 @pytest.fixture
-def correlations_array(antenna_pairs):
+def pol_correlations_array(antenna_pairs):
     real = NP.arange(len(antenna_pairs)*2*2)
     imag = real[::-1]
     return (real+1j*imag).reshape(-1,2,2)
@@ -52,7 +52,7 @@ def polaxes():
     return (-2,-1)
 
 @pytest.fixture
-def corrs_list1():
+def pol_corrs_list1():
     return [NP.array([[1+2j, 3+4j], 
                       [5+6j, 7+8j]]), 
             NP.array([[9+10j, 11+12j], 
@@ -61,7 +61,7 @@ def corrs_list1():
                       [21+22j, 23+24j]])]
 
 @pytest.fixture
-def corrs_list2():
+def pol_corrs_list2():
     return [NP.array([[25+26j, 27+28j], 
                       [29+30j, 31+32j]]), 
             NP.array([[33+34j, 35+36j], 
@@ -70,7 +70,7 @@ def corrs_list2():
                       [45+46j, 47+48j]])]
 
 @pytest.fixture
-def corrs_list3():
+def pol_corrs_list3():
     return [NP.array([[49+50j, 51+52j], 
                       [53+54j, 55+56j]]), 
             NP.array([[57+58j, 59+60j], 
@@ -79,27 +79,27 @@ def corrs_list3():
                       [69+70j, 71+72j]])]
 
 @pytest.fixture
-def advariant1(corrs_list1):
-    return (corrs_list1[0]@NP.linalg.inv(corrs_list1[1].T.conj())@corrs_list1[2])[NP.newaxis,...]
+def pol_advariant1(pol_corrs_list1):
+    return (pol_corrs_list1[0]@NP.linalg.inv(pol_corrs_list1[1].T.conj())@pol_corrs_list1[2])[NP.newaxis,...]
 
 @pytest.fixture
-def advariant2(corrs_list2):
-    return (corrs_list2[0]@NP.linalg.inv(corrs_list2[1].T.conj())@corrs_list2[2])[NP.newaxis,...]
+def pol_advariant2(pol_corrs_list2):
+    return (pol_corrs_list2[0]@NP.linalg.inv(pol_corrs_list2[1].T.conj())@pol_corrs_list2[2])[NP.newaxis,...]
 
 @pytest.fixture
-def advariant3(corrs_list3):
-    return (corrs_list3[0]@NP.linalg.inv(corrs_list3[1].T.conj())@corrs_list3[2])[NP.newaxis,...]
+def pol_advariant3(pol_corrs_list3):
+    return (pol_corrs_list3[0]@NP.linalg.inv(pol_corrs_list3[1].T.conj())@pol_corrs_list3[2])[NP.newaxis,...]
 
 @pytest.fixture
-def corrs_lol(corrs_list1, corrs_list2, corrs_list3):
-    return [corrs_list1, corrs_list2, corrs_list3]
+def pol_corrs_lol(pol_corrs_list1, pol_corrs_list2, pol_corrs_list3):
+    return [pol_corrs_list1, pol_corrs_list2, pol_corrs_list3]
 
 @pytest.fixture
-def advariants_on_list(advariant1, advariant2, advariant3):
-    return NP.concatenate([advariant1, advariant2, advariant3], axis=0)[NP.newaxis,...]
+def pol_advariants_on_list(pol_advariant1, pol_advariant2, pol_advariant3):
+    return NP.concatenate([pol_advariant1, pol_advariant2, pol_advariant3], axis=0)[NP.newaxis,...]
 
 @pytest.fixture
-def vectors_from_advariants():
+def vectors_from_pol_advariants():
     return NP.asarray([[[ -8.5 +5.j,  -8.5 +5.j, 3.5+3.5j, 4.5-4.5j],
                         [-44.5+41.j, -44.5+41.j, 3.5+3.5j, 4.5-4.5j],
                         [-80.5+77.j, -80.5+77.j, 3.5+3.5j, 4.5-4.5j]]])
@@ -120,7 +120,7 @@ def minkoski_dots_scaling_factor_removed(complete_minkowski_dots):
     return complete_minkowski_dots / NP.sqrt(NP.sum(complete_minkowski_dots**2, axis=-1, keepdims=True))
 
 @pytest.fixture
-def complex_gains():
+def pol_complex_gains():
     nants = 10
     mean_gain_scale = 3.0
     randseed = None
--- ClosureInvariants/tests/test_vectorInvariants.py
+++ ClosureInvariants/tests/test_vectorInvariants.py
@@ -2,20 +2,20 @@ import pytest
 import numpy as NP
 from .. import vectorInvariants as VI
 
-def test_corrs_list_on_loops_shape(correlations_array, antenna_pairs, loops):
-    result = VI.corrs_list_on_loops(correlations_array, antenna_pairs, loops)
+def test_pol_corrs_list_on_loops_shape(pol_correlations_array, antenna_pairs, loops):
+    result = VI.corrs_list_on_loops(pol_correlations_array, antenna_pairs, loops)
     assert len(result) == len(loops)
     for corr_loop in result:
         assert len(corr_loop) == len(loops[0])
 
-def test_corrs_list_on_loops_type(correlations_array, antenna_pairs, loops):
-    result = VI.corrs_list_on_loops(correlations_array, antenna_pairs, loops)
+def test_pol_corrs_list_on_loops_type(pol_correlations_array, antenna_pairs, loops):
+    result = VI.corrs_list_on_loops(pol_correlations_array, antenna_pairs, loops)
     for corr_loop in result:
         for corr in corr_loop:
             assert isinstance(corr, NP.ndarray)
 
-def test_corrs_list_on_loops_values(correlations_array, antenna_pairs, loops):
-    result = VI.corrs_list_on_loops(correlations_array, antenna_pairs, loops)
+def test_pol_corrs_list_on_loops_values(pol_correlations_array, antenna_pairs, loops):
+    result = VI.corrs_list_on_loops(pol_correlations_array, antenna_pairs, loops)
     
     for loop, corr_loop in zip(loops, result):
         for i in range(len(loop)):
@@ -28,53 +28,53 @@ def test_corrs_list_on_loops_values(correlations_array, antenna_pairs, loops):
                 conj = True
             else:
                 assert False, "Loop antenna pair {0} not found in input antenna pairs".format(antpair_in_loop)
-            inpcorr = correlations_array[pair_index]
+            inpcorr = pol_correlations_array[pair_index]
             if conj:
                 inpcorr = inpcorr.T.conj()
             assert NP.allclose(corr_loop[i], inpcorr)
 
-def test_advariant(corrs_list1, polaxes, advariant1):
+def test_pol_advariant(pol_corrs_list1, polaxes, pol_advariant1):
     # Call the function
-    result = VI.advariant(corrs_list1, pol_axes=polaxes)
+    result = VI.advariant(pol_corrs_list1, pol_axes=polaxes)
 
     # Check if the result is a numpy array
     assert isinstance(result, NP.ndarray)    
 
     # Check if the result has the expected shape
-    assert result.shape == advariant1.shape
+    assert result.shape == pol_advariant1.shape
 
     # Check if the values are as expected
-    assert NP.allclose(result, advariant1)
+    assert NP.allclose(result, pol_advariant1)
 
 @pytest.mark.parametrize("expected_shape", [(1, 2, 2, 2)])
-def test_advariants_multiple_loops(corrs_lol, expected_shape, advariants_on_list):
+def test_pol_advariants_multiple_loops(pol_corrs_lol, expected_shape, pol_advariants_on_list):
     # Call the function
-    result = VI.advariants_multiple_loops(corrs_lol)
+    result = VI.advariants_multiple_loops(pol_corrs_lol)
 
     # Check if the result is a numpy array
     assert isinstance(result, NP.ndarray)    
     
     # Check if the result has the expected shape
-    assert result.shape == advariants_on_list.shape
+    assert result.shape == pol_advariants_on_list.shape
 
     # Check if the values are as expected
-    assert NP.allclose(result, advariants_on_list)
+    assert NP.allclose(result, pol_advariants_on_list)
 
-def test_vectors_from_advariants(advariants_on_list, vectors_from_advariants):
-    result = VI.vector_from_advariant(advariants_on_list)
+def test_vectors_from_advariants(pol_advariants_on_list, vectors_from_pol_advariants):
+    result = VI.vector_from_advariant(pol_advariants_on_list)
 
     # Check if the result is a numpy array
     assert isinstance(result, NP.ndarray)    
     
     # Check if the result has the expected shape
-    assert result.shape == vectors_from_advariants.shape
+    assert result.shape == vectors_from_pol_advariants.shape
 
     # Check if the values are as expected
-    assert NP.allclose(result, vectors_from_advariants)
+    assert NP.allclose(result, vectors_from_pol_advariants)
 
-def test_minkowski_dot_products(vectors_from_advariants, minkowski_dot_products):
-    z2 = vectors_from_advariants[:,:2,:]
-    z1 = vectors_from_advariants[:,[2],:]
+def test_minkowski_dot_products(vectors_from_pol_advariants, minkowski_dot_products):
+    z2 = vectors_from_pol_advariants[:,:2,:]
+    z1 = vectors_from_pol_advariants[:,[2],:]
     mdp22 = VI.minkowski_dot(z2)
     mdp21 = VI.minkowski_dot(z2, z1)
     mdp22_expected, mdp21_expected = minkowski_dot_products
@@ -83,10 +83,10 @@ def test_minkowski_dot_products(vectors_from_advariants, minkowski_dot_products)
     assert NP.allclose(mdp22, mdp22_expected)
     assert NP.allclose(mdp21, mdp21_expected)
 
-def test_complete_minkowski_dots(vectors_from_advariants, complete_minkowski_dots):
-    result = VI.complete_minkowski_dots(vectors_from_advariants)
+def test_complete_minkowski_dots(vectors_from_pol_advariants, complete_minkowski_dots):
+    result = VI.complete_minkowski_dots(vectors_from_pol_advariants)
     assert result.size == complete_minkowski_dots.size
-    assert result.size == (4*5)//2 + 2*(vectors_from_advariants.shape[-2]-2)*4
+    assert result.size == (4*5)//2 + 2*(vectors_from_pol_advariants.shape[-2]-2)*4
     assert NP.allclose(result, complete_minkowski_dots)    
 
 def test_remove_scaling_factor_minkoski_dots(complete_minkowski_dots, minkoski_dots_scaling_factor_removed):
@@ -94,8 +94,8 @@ def test_remove_scaling_factor_minkoski_dots(complete_minkowski_dots, minkoski_d
     assert NP.allclose(result, minkoski_dots_scaling_factor_removed)
     assert NP.allclose(NP.sum(result**2), 1.0)
 
-def test_invariance(corrs_list1, corrs_list2, corrs_list3, complex_gains):
-    corrs_in = [corrs_list1, corrs_list2, corrs_list3]
+def test_invariance(pol_corrs_list1, pol_corrs_list2, pol_corrs_list3, pol_complex_gains):
+    corrs_in = [pol_corrs_list1, pol_corrs_list2, pol_corrs_list3]
     advars_in = VI.advariants_multiple_loops(corrs_in)
     z4v_in = VI.vector_from_advariant(advars_in)
     mdp_in = VI.complete_minkowski_dots(z4v_in)
@@ -103,7 +103,7 @@ def test_invariance(corrs_list1, corrs_list2, corrs_list3, complex_gains):
 
     preinds = NP.concatenate([NP.zeros((3,1), dtype=int), 1+NP.arange(6, dtype=int).reshape(3,2)], axis=-1)
     postinds = NP.roll(preinds, -1, axis=-1)
-    corrs_out = [VI.corrupt_visibilities(NP.array(corrs_in[loopi]), complex_gains[preinds[loopi]], complex_gains[postinds[loopi]]) for loopi in range(len(corrs_in))]
+    corrs_out = [VI.corrupt_visibilities(NP.array(corrs_in[loopi]), pol_complex_gains[preinds[loopi]], pol_complex_gains[postinds[loopi]]) for loopi in range(len(corrs_in))]
     advars_out = VI.advariants_multiple_loops(corrs_out)
     z4v_out = VI.vector_from_advariant(advars_out)
     mdp_out = VI.complete_minkowski_dots(z4v_out)
